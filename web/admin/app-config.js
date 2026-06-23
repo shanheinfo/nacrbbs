190d78180ef515eba138db6e8148f696fc6c2209
shanhel@qq.com <shanhel@qq.com>
2026-06-23 18:16:33 +0800

    chore: 删除废弃的app-config.js - 已迁移到Nuxt runtimeConfig，无任何引用

--- web/admin/app-config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    /* 后端API地址 */
-  apiBase: 'https://api.bbs.nacr.cn'
-}
--- web/client/app-config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    /* 后端API地址 */
-  // apiBase: 'http://192.168.10.157:9999',
-  apiBase: 'https://api.bbs.nacr.cn'
-}
